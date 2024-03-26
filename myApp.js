4946ecf55b08576c637308a57359e4b75f778532
Glitch (certain-accessible-zephyr) <none>
2024-03-26 17:07:58 +0000

    🚶⛰ Checkpoint ./myApp.js:65965251/202

--- myApp.js
+++ myApp.js
@@ -45,6 +45,34 @@ app.use(helmet.dnsPrefetchControl())
 
 
 
+app.use(helmet.noCache())
+
+
+
+
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
+
+
+
+
+app.use(helmet.parent())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
